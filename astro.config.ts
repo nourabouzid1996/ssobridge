11a572e1435fac2381869643986fff44b44c01c2
nourabouzid1996 <nourabouzid1996@gmail.com>
2024-09-16 20:32:02 +0200

    Your commit message

--- astro.config.ts
+++ astro.config.ts
@@ -15,6 +15,8 @@ import astrowind from './vendor/integration';
 
 import { readingTimeRemarkPlugin, responsiveTablesRehypePlugin, lazyImagesRehypePlugin } from './src/utils/frontmatter';
 
+import vercel from '@astrojs/vercel';
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const hasExternalScripts = false;
@@ -87,4 +89,6 @@ export default defineConfig({
       },
     },
   },
+
+  adapter: vercel(),
 });
